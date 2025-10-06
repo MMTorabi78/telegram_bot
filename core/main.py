8c7806860737c481e45f39392f90151126c48b8b
MMTorabi78 <mtorabi450@gmail.com>
2025-10-06 18:33:03 +0330

    download a simple files

--- core/main.py
+++ core/main.py
@@ -2,26 +2,45 @@ import telebot
 import os
 from dotenv import load_dotenv
 import logging
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-
+import requests
 load_dotenv(dotenv_path="./bot_tel/.env")
+
+DOWNLOAD_DIR = "/home/mtorabi/python/bot_tel/downloads/"
+
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
 API_TOKEN = os.environ.get("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    telebot.logger.info("triggered /start command")
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="Choose an option")
-    markup.add(KeyboardButton("about"), KeyboardButton("help"))
-    bot.send_message(message.chat.id, 'hi this is a simple bot for a test', reply_markup=markup)
+    bot.send_message(message.chat.id, "Send a link → Get the file.")
 
-@bot.message_handler(func=lambda message: message.text == "about")
-def about(message):
-    bot.send_message(message.chat.id, "This is a simple test bot created using the Telebot library.")
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    file_path = DOWNLOAD_DIR + local_filename
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
 
-@bot.message_handler(func=lambda message: message.text == "help")
-def help(message):
-    bot.send_message(message.chat.id, "Available commands:\n/start - Start the bot\n/about - About the bot\n/help - Show this help message")
+@bot.message_handler(func=lambda message: True)
+def download_file_url(message):
+    logger.info(message.text)
+    url = message.text
+    try:
+        file_path = download_file(url)
+        bot.send_document(chat_id=message.chat.id, reply_to_message_id=message.message_id, document=open(
+            file_path, "rb"), caption="file downloaded successfully, ENJOY!")
+        os.remove(file_path)
+    except:
+        raise
+        bot.reply_to(message, text="problem downloading the requested file")
 
 bot.infinity_polling()
